121f889ca4889c93b0471979ec2607e7ec2325eb
Jason Marshall <marshallj88@gmail.com>
2017-08-09 21:36:41 -0400

    Update leaflet basemap. Simplify...

--- src/app-client.js
+++
@@ -1,6 +0,0 @@
-import css from './style';
-import React from 'react';
-import { render } from 'react-dom';
-import AppRoutes from './components/AppRoutes';
-
-render(<AppRoutes />, document.getElementById('app'));
