9dce13bdad445ffec678430660160faa10713162
Jason Marshall <marshallj88@gmail.com>
2017-08-11 11:24:51 -0400

    Style leaflet map to full window

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import MapContainer from './MapContainer';
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="map">
         <MapContainer />
       </div>
     );
