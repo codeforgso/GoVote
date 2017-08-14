009f1bb11a38b08dc7de7e820fe707d26d55a6b4
Jason Marshall <marshallj88@gmail.com>
2017-08-13 21:44:59 -0400

    API cleanup

--- server/index.js
+++ server/index.js
@@ -20,12 +20,12 @@ const client = new Client({
 client.connect();
 
 router.get('/:fn/:ln', async (req, res) => {
-  const { rows } = await client.query("SELECT * FROM GuilfordVoters WHERE first_name ilike $1::text and last_name ilike $2::text", [req.params.fn, req.params.ln]);
+  const query = `SELECT * FROM ${process.env.DB_TABLE} WHERE first_name ilike $1::text and last_name ilike $2::text`;
+  const { rows } = await client.query(query, [req.params.fn, req.params.ln]);
   res.send(rows);
 });
 
 app.use(router);
-
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`);
