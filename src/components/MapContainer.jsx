8399988cdc607c3c9a5a8c169ebfc0fe55901acb
Jason Marshall <marshallj88@gmail.com>
2017-08-18 21:12:28 -0400

    Update pin popup text

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -5,14 +5,14 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 const position = [36.0726, -79.7920];
 const MapContainer = (props) => {
   return (
-    <Map className="map" center={position} zoom={13}>
+    <Map className="map" center={position} zoom={12}>
       <TileLayer
         url='http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
       />
       <Marker position={position}>
         <Popup>
-          <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+          <span>I am <a href='http://govotegso.org' target='blank'>GoVoteGSO</a><br/>Help Code for Greensboro make me awesome!</span>
         </Popup>
       </Marker>
     </Map>
