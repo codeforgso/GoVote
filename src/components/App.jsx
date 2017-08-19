f02acd879bdae3631cb333837d329a722658bc3b
Jason Marshall <marshallj88@gmail.com>
2017-08-19 11:27:52 -0400

    Voter Registration modal implemented. No data.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MapContainer from './MapContainer';
+import VoterModal from './VoterModal.jsx';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      modalShow: true,
+    };
+  }
+
   render() {
+    const modalClose = () => this.setState({ modalShow: false });
     return (
       <div className="map">
         <MapContainer />
+        <VoterModal show={this.state.modalShow} onHide={modalClose} />
       </div>
     );
   }
---
+++ src/components/VoterModal.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Modal, Button, Form, FormControl, FormGroup, ControlLabel } from 'react-bootstrap';
+
+const VoterModal = (props) => {
+  const {
+    onHide,
+    show,
+  } = props;
+
+  return (
+    <Modal {...props} bsSize="lg" aria-labelledby="contained-modal-title-lg">
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-lg">
+          GoVoteGSO
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="county-modal-list">
+          <h4>Enter first and last name for your current voter registration status.</h4>
+          <Form inline>
+            <FormGroup controlId="formFirstName" validationState={null}>
+              <ControlLabel>First Name</ControlLabel>
+              {' '}
+              <FormControl type="text" placeholder="Jane" />
+            </FormGroup>
+            {' '}
+            <FormGroup controlId="formLastName" validationState={null}>
+              <ControlLabel>Last Name</ControlLabel>
+              {' '}
+              <FormControl type="text" placeholder="Doe" />
+            </FormGroup>
+            {' '}
+            <Button type="submit">
+              Search
+            </Button>
+          </Form>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={onHide}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+VoterModal.propTypes = {
+  onHide: PropTypes.func.isRequired,
+  show: PropTypes.bool.isRequired,
+};
+
+module.exports = VoterModal;
