d04951972a5bd83c89a521ff229d6194fad23f89
SchaeStewart <SchaeStewart@gmail.com>
2017-08-28 17:59:42 -0400

    Add address verification to VoterModal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,79 +1,95 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import MapContainer from './MapContainer';
 import VoterModal from './VoterModal.jsx';
 import axios from 'axios';
-
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      modalShow: true,
-      firstName: null,
-      lastName: null,
-      voterInfo: {},
-      layers: {
-        councilDist: null,
-        commissionerDist: null
-      }
-    };
-    this._handleInputChange = this._handleInputChange.bind(this);
-  }
+    constructor() {
+        super();
+        this.state = {
+            modalShow: true,
+            firstName: null,
+            lastName: null,
+            voterInfo: [],
+            layers: {
+                councilDist: null,
+                commissionerDist: null
+            },
+            selectedRadio: {},
+            showUserStatus: false,
+        };
+        this._handleInputChange = this._handleInputChange.bind(this);
+    }
 
-  componentWillMount() {
-    this._getGISData(this.props.match);
-  }
+    componentWillMount() {
+        this._getGISData(this.props.match);
+    }
 
-  _getCouncilDistricts = () => {
-    return axios.get('http://data-greensboro.opendata.arcgis.com/datasets/829c58aaaf0c4bf0b59f93bfe3cb4c13_3.geojson');
-  }
+    _getCouncilDistricts = () => {
+        return axios.get('http://data-greensboro.opendata.arcgis.com/datasets/829c58aaaf0c4bf0b59f93bfe3cb4c13_3.geojson');
+    }
 
-  _getCommissionerDistricts = () => {
-    return axios.get('http://data-greensboro.opendata.arcgis.com/datasets/1b60f15bb4dc4d8f96bd4831a8fbf063_5.geojson');
-  }
+    _getCommissionerDistricts = () => {
+        return axios.get('http://data-greensboro.opendata.arcgis.com/datasets/1b60f15bb4dc4d8f96bd4831a8fbf063_5.geojson');
+    }
 
-  _getGISData = () => {
-    axios.all([this._getCouncilDistricts(), this._getCommissionerDistricts()])
-    .then(axios.spread((councilDist, commissionerDist) => {
-      console.log(councilDist.data, commissionerDist.data);
-      this.setState({ layers: { councilDist: councilDist.data, commissionerDist: commissionerDist.data } });
-    }));
-  }
+    _getGISData = () => {
+        axios.all([this._getCouncilDistricts(), this._getCommissionerDistricts()])
+            .then(axios.spread((councilDist, commissionerDist) => {
+                console.log(councilDist.data, commissionerDist.data);
+                this.setState({layers: {councilDist: councilDist.data, commissionerDist: commissionerDist.data}});
+            }));
+    }
 
-  _getVoterInfo = () => {
-    axios.get(`/api/${this.state.firstName}/${this.state.lastName}`, { baseURL: 'http://localhost:3001/' })
-    .then((response) => {
-      this.setState({voterInfo: response});
-      console.log(response);
-      alert(JSON.stringify(this.state.voterInfo.data, null, 2));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
+    _getVoterInfo = () => {
+        axios.get(`/api/${this.state.firstName}/${this.state.lastName}`, {baseURL: 'http://localhost:3001/'})
+            .then((response) => {
+                this.setState({voterInfo: response.data});
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
-  _handleInputChange = (event) => {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+    _handleRadioClick = (el) => {
+        this.setState({
+            selectedRadio: el,
+            showUserStatus: false,
+        });
+    }
 
-    this.setState({
-      [name]: value
-    });
-  }
+    _handleRadioSubmit = () => {
+        this.setState({
+            showUserStatus: true,
+        });
+    }
 
-  render() {
-    const modalClose = () => this.setState({ modalShow: false });
-    return (
-      this.state.layers.councilDist && this.state.layers.commissionerDist ?
-        <div className="map">
-          <MapContainer data={this.state.layers} />
-          <VoterModal show={this.state.modalShow} onHide={modalClose} onSubmit={this._getVoterInfo} onUpdate={this._handleInputChange}/>
-        </div> : null
-    );
-  }
+    _handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        const modalClose = () => this.setState({modalShow: false});
+        return (
+            this.state.layers.councilDist && this.state.layers.commissionerDist ?
+                <div className="map">
+                    <MapContainer data={this.state.layers}/>
+                    <VoterModal show={this.state.modalShow} onHide={modalClose} onSubmit={this._getVoterInfo}
+                                onUpdate={this._handleInputChange} voterInfo={this.state.voterInfo}
+                                _handleRadioSubmit={this._handleRadioSubmit}
+                                _handleRadioClick={this._handleRadioClick} selectedRadio={this.state.selectedRadio}
+                                showUserStatus={this.state.showUserStatus}/>
+                </div> : null
+        );
+    }
 }
 
 App.propTypes = {
-  match: PropTypes.object.isRequired,
+    match: PropTypes.object.isRequired,
 };
--- src/components/VoterModal.jsx
+++ src/components/VoterModal.jsx
@@ -1,52 +1,94 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Modal, Button, Form, FormControl, FormGroup, ControlLabel } from 'react-bootstrap';
+import {
+    Modal,
+    Button,
+    Form,
+    FormControl,
+    FormGroup,
+    Radio,
+    ControlLabel
+} from 'react-bootstrap';
 
 const VoterModal = (props) => {
-  const {
-    onHide,
-    show,
-    onSubmit,
-    onUpdate
-  } = props;
+    const {
+        onHide,
+        show,
+        onSubmit,
+        onUpdate,
+        voterInfo,
+        _handleRadioClick,
+        _handleRadioSubmit,
+        selectedRadio,
+        showUserStatus,
+    } = props;
 
-  return (
-    <Modal {...props} bsSize="lg" aria-labelledby="contained-modal-title-lg">
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-lg">
-          GoVoteGSO
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="county-modal-list">
-          <h4>Enter first and last name for your current voter registration status.</h4>
-          <Form inline>
-            <FormGroup controlId="formFirstName" validationState={null}>
-              <ControlLabel>First Name</ControlLabel>
-              <FormControl type="text" name="firstName" onChange={onUpdate} placeholder="Jane" />
-            </FormGroup>
-            <FormGroup controlId="formLastName" validationState={null}>
-              <ControlLabel>Last Name</ControlLabel>
-              <FormControl type="text" name="lastName" onChange={onUpdate} placeholder="Doe" />
-            </FormGroup>
-            <Button type="button" onClick={onSubmit} >
-              Search
-            </Button>
-          </Form>
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={onHide}>
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+    const renderVoterInfo = (el) => {
+        return (
+            <Radio name="voterRadioGroup" key={el.voter_reg_num}
+                   onClick={() => _handleRadioClick(el)}>{el.resident_address}</Radio>
+        );
+    }
+
+    const renderUserStatus = (status) => {
+        // Capitalizes the first letter of the string and converts the rest to lowercase
+        status = status[0] + status.substring(1).toLowerCase();
+        return (
+            <div>{status}</div>
+        );
+    }
+    return (
+        <Modal show={show} bsSize="lg" aria-labelledby="contained-modal-title-lg">
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-lg">
+                    GoVoteGSO
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="county-modal-list">
+                    <h4>Enter first and last name for your current voter registration status.</h4>
+                    <Form inline>
+                        <FormGroup controlId="formFirstName" validationState={null}>
+                            <ControlLabel>First Name</ControlLabel>
+                            <FormControl type="text" name="firstName" onChange={onUpdate} placeholder="Jane"/>
+                        </FormGroup>
+                        <FormGroup controlId="formLastName" validationState={null}>
+                            <ControlLabel>Last Name</ControlLabel>
+                            <FormControl type="text" name="lastName" onChange={onUpdate} placeholder="Doe"/>
+                        </FormGroup>
+                        <Button type="button" onClick={onSubmit}>
+                            Search
+                        </Button>
+                    </Form>
+                    <FormGroup>
+                        {voterInfo.map(renderVoterInfo)}
+                        {
+                            voterInfo.length ?
+                                <Button onClick={_handleRadioSubmit}>
+                                    This is my address
+                                </Button>
+                                : null
+                        }
+                        {
+                            showUserStatus ?
+                                renderUserStatus(selectedRadio.voter_status_desc)
+                                : null
+                        }
+                    </FormGroup>
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={onHide}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 };
 
 VoterModal.propTypes = {
-  onHide: PropTypes.func.isRequired,
-  show: PropTypes.bool.isRequired,
+    onHide: PropTypes.func.isRequired,
+    show: PropTypes.bool.isRequired
 };
 
 module.exports = VoterModal;
