23ceed85fbbe1b74644ee8ea3b4a70981f87dc5e
Kristen Goehringer <kgoehringer@gmail.com>
2017-11-06 16:17:19 -0500

    Added copy and logo to the AboutModal.  Added CSS to the logo and button in AboutModal footer.

--- src/components/AboutModal.jsx
+++ src/components/AboutModal.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Modal, Button } from 'react-bootstrap';
+import {
+  Modal,
+  Button,
+  Thumbnail,
+} from 'react-bootstrap';
+import cfgLogo from '../static/CfGLogoWhite.png';
 
 const AboutModal = (props) => {
   const {
@@ -15,13 +20,18 @@ const AboutModal = (props) => {
           <Modal.Title>About Code for Greensboro</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h3>Rerum in aut aut debitis aliquid possimus qui.</h3>
-          <p>Quas maiores pariatur magnam dolores ut beatae id voluptas.</p>
-          <p>Culpa amet enim quaerat esse aut.</p>
-          <p>Consequatur voluptatem omnis fugit nisi. Soluta nulla provident repellat id corporis. Rem sit doloremque. Quo facere alias.</p>
+          <h4>Code for Greensboro works to improve the way our community uses its data and the web.  And we need <strong>you</strong>—no coding required.</h4>
+          <br/>
+          <p>We are a local 501(c)(3) organization affiliated with the Code for America Brigade program.
+            Code for America brigades are volunteer civic tech communities that collaborate with local government and community partners to build open source technology websites, and tools.  
+            These projects provide greater access to, or augment existing government services, and otherwise help with local civic issues.  
+            Code for America supports Brigade chapters with resources, tools, and access to the wider civic technology movement.</p>
+          <p>We are developers, designers, data nerds, civically - minded people, and residents — in other words, we are this community.  
+            We want to use this network and these talents to help local government, and the community create positive impact through technology.</p>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={hide} bsStyle="default">Close</Button>
+          <Thumbnail href="http://codeforgreensboro.org" target="_blank" src={cfgLogo} className="logo about-footer-logo" />
+          <Button onClick={hide} bsStyle="default" className="about-footer-btn">Close</Button>
         </Modal.Footer>
       </Modal>
 
