1544ebdd2d89feca85dcf1f14c153c829077c465
Blake Guilloud <blake.guilloud@gmail.com>
2018-02-06 19:02:07 -0500

    Blake - lockin bootstrap version to 3.3.7

--- src/components/AboutModal.jsx
+++ src/components/AboutModal.jsx
@@ -21,12 +21,12 @@ const AboutModal = (props) => {
         </Modal.Header>
         <Modal.Body>
           <h4>Code for Greensboro works to improve the way our community uses its data and the web.  And we need <strong>you</strong>—no coding required.</h4>
-          <br/>
+          <br />
           <p>We are a local 501(c)(3) organization affiliated with the Code for America Brigade program.
-            Code for America brigades are volunteer civic tech communities that collaborate with local government and community partners to build open source technology websites, and tools.  
-            These projects provide greater access to, or augment existing government services, and otherwise help with local civic issues.  
+            Code for America brigades are volunteer civic tech communities that collaborate with local government and community partners to build open source technology websites, and tools.
+            These projects provide greater access to, or augment existing government services, and otherwise help with local civic issues.
             Code for America supports Brigade chapters with resources, tools, and access to the wider civic technology movement.</p>
-          <p>We are developers, designers, data nerds, civically - minded people, and residents — in other words, we are this community.  
+          <p>We are developers, designers, data nerds, civically - minded people, and residents — in other words, we are this community.
             We want to use this network and these talents to help local government, and the community create positive impact through technology.</p>
         </Modal.Body>
         <Modal.Footer>
