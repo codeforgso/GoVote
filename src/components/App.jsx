3e965e716d372defd12589cb29b498a74fd03efe
Blake Guilloud <blake.guilloud@gmail.com>
2018-03-21 19:26:01 -0400

    Blake - separation of concerns. General application cleanup.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,52 @@
-/* eslint-disable no-console */
 import React, { Component } from 'react';
-import axios from 'axios';
 import PropTypes from 'prop-types';
-import MapContainer from './MapContainer';
-import VoterModal from './VoterModal.jsx';
-import VoterInfoForm from './VoterInfoForm';
-import VerifyVoterInfo from './VerifyVoterInfo';
-import RegistrationInfoModal from './RegistrationInfoModal';
-import Header from './Header';
+
+import { getGISData, getVoterInfo } from '../actions';
 import AboutModal from './AboutModal';
+import Header from './Header';
+import MapContainer from './MapContainer';
+import RegistrationInfoModal from './RegistrationInfoModal';
+import VerifyVoterInfo from './VerifyVoterInfo';
+import VoterInfoForm from './VoterInfoForm';
+import VoterModal from './VoterModal';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      voterModalShow: true,
-      regInfoModalShow: false,
-      aboutModalShow: false,
-      firstName: '',
-      lastName: '',
-      voterInfo: [],
-      voterAddress: '',
-      layers: {
-        councilDist: null,
-        commissionerDist: null,
-      },
-      firstNameValidationState: null,
-      lastNameValidationState: null,
-      formErrors: [],
-      voterInfoFound: true,
-    };
-    this._handleInputChange = this._handleInputChange.bind(this);
-  }
+  state = {
+    voterModalShow: true,
+    regInfoModalShow: false,
+    aboutModalShow: false,
+    firstName: '',
+    lastName: '',
+    voterInfo: [],
+    voterAddress: '',
+    layers: {
+      councilDist: null,
+      commissionerDist: null,
+    },
+    firstNameValidationState: null,
+    lastNameValidationState: null,
+    formErrors: [],
+    voterInfoFound: true,
+  };
 
-  componentWillMount() {
-    this._getGISData(this.props.match);
-  }
-
-  _getCouncilDistricts = () => axios.get('http://data-greensboro.opendata.arcgis.com/datasets/829c58aaaf0c4bf0b59f93bfe3cb4c13_3.geojson')
-
-  _getCommissionerDistricts = () => axios.get('http://data-greensboro.opendata.arcgis.com/datasets/1b60f15bb4dc4d8f96bd4831a8fbf063_5.geojson')
-
-  _getGISData = () => {
-    axios.all([this._getCouncilDistricts(), this._getCommissionerDistricts()])
-      .then(axios.spread((councilDist, commissionerDist) => {
-        this.setState({ layers: { councilDist: councilDist.data, commissionerDist: commissionerDist.data } });
-      }));
+  componentDidMount() {
+    getGISData()
+      .then(layers => this.setState({ layers }));
   }
 
   _getVoterInfo = () => {
     if (this._validateVoterInput()) {
-      this.setState({ firstNameValidationState: null });
-      this.setState({ lastNameValidationState: null });
-      axios.get(`/api/${this.state.firstName}/${this.state.lastName}`)
-        .then((response) => {
+      this.setState({
+        firstNameValidationState: null,
+        lastNameValidationState: null,
+      });
+
+      getVoterInfo(this.state.firstName, this.state.lastName)
+        .then((voterInfo) => {
           this.setState({
-            voterInfo: response.data,
-            voterInfoFound: response.data.length > 0,
+            voterInfo,
+            voterInfoFound: voterInfo.length > 0,
           });
-        })
-        .catch((error) => {
-          console.log(error);
         });
     } else {
       this.setState({ voterInfo: [] });
@@ -85,7 +71,7 @@ class App extends Component {
   }
 
   _getVoterAddress = (address) => {
-    this.setState({ voterAddress: address }, () => console.log(this.state.voterAddress));
+    this.setState({ voterAddress: address });
   }
 
   _showVoterInfoModal = () => {
@@ -104,9 +90,7 @@ class App extends Component {
   }
 
   _handleInputChange = (event) => {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+    const { name, value } = event.target;
 
     this.setState({
       [name]: value,
@@ -117,17 +101,11 @@ class App extends Component {
   }
 
   _handleShowAboutModal = () => {
-    this.setState({
-      aboutModalShow: true,
-    });
-    console.log('clicked');
+    this.setState({ aboutModalShow: true });
   }
 
   _handleHideAboutModal = () => {
-    this.setState({
-      aboutModalShow: false,
-    });
-    console.log('closed');
+    this.setState({ aboutModalShow: false });
   }
 
   render() {
--- src/components/VoterModal.jsx
+++ src/components/VoterModal.jsx
@@ -2,32 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Modal, Button } from 'react-bootstrap';
 
-const VoterModal = (props) => {
-  const {
-      hide,
-      show,
-  } = props;
-
-  return (
-    <Modal show={show} onHide={hide} bsSize="sm" aria-labelledby="contained-modal-title-lg">
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-lg">
-          GoVoteGSO
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="county-modal-list">
-          {props.children}
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={hide}>
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
+const VoterModal = ({ hide, show, children }) => (
+  <Modal show={show} onHide={hide} aria-labelledby="contained-modal-title-lg">
+    <Modal.Header closeButton>
+      <Modal.Title id="contained-modal-title-lg">
+        GoVoteGSO
+      </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <div className="county-modal-list">
+        {children}
+      </div>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button onClick={hide}>
+        Close
+      </Button>
+    </Modal.Footer>
+  </Modal>
   );
-};
 
 VoterModal.propTypes = {
   hide: PropTypes.func.isRequired,
@@ -35,4 +28,4 @@ VoterModal.propTypes = {
   children: PropTypes.array.isRequired,
 };
 
-module.exports = VoterModal;
+export default VoterModal;
