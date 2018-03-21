826bd7212f5764eb48723030713752be2a46047f
Blake Guilloud <blake.guilloud@gmail.com>
2018-03-21 13:40:18 -0400

    Blake - alias pg-native to export null.. Strange workaround

--- config/webpackProdServer.config.js
+++ config/webpackProdServer.config.js
@@ -1,27 +1,29 @@
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
   entry: './server/server.js',
   output: {
     path: path.resolve(__dirname, '../build'),
-    filename: 'server.js'
+    filename: 'server.js',
   },
   devtool: 'source-map',
   stats: {
-         colors: true
-     },
+    colors: true,
+  },
   target: 'node',
+
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'stage-0']
-        }
-      }
-    ]
-  }
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      'pg-native': path.join(__dirname, 'aliases/pg-native.js'),
+    },
+  },
 };
