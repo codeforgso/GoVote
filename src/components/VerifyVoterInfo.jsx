c69ac3c96568965e0d51e689c451dee35b9198f7
Rob Martin <rob.martin.5758@gmail.com>
2018-07-10 19:55:57 -0400

    #87 moved address above voter info when address selected

--- src/components/VerifyVoterInfo.jsx
+++ src/components/VerifyVoterInfo.jsx
@@ -8,7 +8,7 @@ class VerifyVoterInfo extends Component {
     this.state = {
       selectedAddress: {},
       showUserStatus: false,
-      mayoralCandidates: ['Nancy Vaughan (i)'],
+      mayoralCandidates: ['Nancy Vaughan (i)']
     };
   }
 
@@ -22,10 +22,10 @@ class VerifyVoterInfo extends Component {
     this.props.voterAddress(el.resident_address);
     this.setState({
       selectedAddress: el,
-      showUserStatus: true,
+      showUserStatus: true
     });
     window.scrollTo(0, 0);
-  }
+  };
 
   _renderUserStatus = (status) => {
     const upperStatus = status.toUpperCase();
@@ -34,62 +34,73 @@ class VerifyVoterInfo extends Component {
         <b>{upperStatus}</b>
       </div>
     );
-  }
+  };
 
   _renderVoterInfo = el => (
-    <Button name="voterAddressGroup" key={el.voter_reg_num} onClick={() => this._handleAddressClick(el)}>{el.resident_address}</Button>
-  )
+    <Button
+      name="voterAddressGroup"
+      key={el.voter_reg_num}
+      onClick={() => this._handleAddressClick(el)}
+    >
+      {el.resident_address}
+    </Button>
+  );
 
-  _renderCandidates = candidate => (
-    <p>{candidate}</p>
-  )
+  _renderCandidates = candidate => <p>{candidate}</p>;
 
   render() {
     return (
       <FormGroup>
-        {
-        this.props.voterInfo.length ?
-          <h4>Verify Your Address:</h4>
-          : null
-        },
-        {
-          this.state.showUserStatus ?
-            <div>
-              <br />
-              {this._renderUserStatus(`VOTER REGISTRATION STATUS: ${this.state.selectedAddress.voter_status_desc}`)}
-            </div>
-            : null
-        }
-        {
-          this.state.showUserStatus && this.state.selectedAddress.ward_abbrv ?
-            <div>
-              <br />
-              {this._renderUserStatus(`GREENSBORO CITY COUNCIL DISTRICT: ${this.state.selectedAddress.ward_abbrv.substring(2, 3)}`)}
-              <br />
-              <b>YOUR GREENSBORO CITY COUNCIL CANDIDATES:</b>
-              <br />
-              <br />
-              <b>Mayoral:</b>
-              {this.state.mayoralCandidates.map(this._renderCandidates)}
-              <br />
-            </div>
-            : null
-        }
-        {
-          this.state.showUserStatus && !this.state.selectedAddress.ward_abbrv ?
-            <div>
-              <br />
-              {this._renderUserStatus('NOT WITHIN GREENSBORO CITY LIMIT')}
-            </div>
-            : null
-        }
+        {this.props.voterInfo.length ? <h4>Verify Your Address:</h4> : null}
+        {this.state.showUserStatus ? (
+          <div>
+            <br />
+            {this._renderUserStatus(
+              `ADDRESS SELECTED: ${this.state.selectedAddress.mail_addr1}`
+            )}
+          </div>
+        ) : null}
+        {this.state.showUserStatus ? (
+          <div>
+            <br />
+            {this._renderUserStatus(
+              `VOTER REGISTRATION STATUS: ${
+                this.state.selectedAddress.voter_status_desc
+              }`
+            )}
+          </div>
+        ) : null}
+        {this.state.showUserStatus && this.state.selectedAddress.ward_abbrv ? (
+          <div>
+            <br />
+            {this._renderUserStatus(
+              `GREENSBORO CITY COUNCIL DISTRICT: ${this.state.selectedAddress.ward_abbrv.substring(
+                2,
+                3
+              )}`
+            )}
+            <br />
+            <b>YOUR GREENSBORO CITY COUNCIL CANDIDATES:</b>
+            <br />
+            <br />
+            <b>Mayoral:</b>
+            {this.state.mayoralCandidates.map(this._renderCandidates)}
+            <br />
+          </div>
+        ) : null}
+        {this.state.showUserStatus && !this.state.selectedAddress.ward_abbrv ? (
+          <div>
+            <br />
+            {this._renderUserStatus('NOT WITHIN GREENSBORO CITY LIMIT')}
+          </div>
+        ) : null}
         <ButtonGroup vertical>
           {this.props.voterInfo.map(this._renderVoterInfo)}
-          {
-            this.props.voterInfo.length ?
-              <Button name="notMyAddress" onClick={this.props.showRegInfoModal}>{'I DON\'T SEE MY ADDRESS'}</Button>
-              : null
-          }
+          {this.props.voterInfo.length ? (
+            <Button name="notMyAddress" onClick={this.props.showRegInfoModal}>
+              {"I DON'T SEE MY ADDRESS"}
+            </Button>
+          ) : null}
         </ButtonGroup>
       </FormGroup>
     );
@@ -99,7 +110,7 @@ class VerifyVoterInfo extends Component {
 VerifyVoterInfo.propTypes = {
   voterInfo: PropTypes.array.isRequired,
   showRegInfoModal: PropTypes.func,
-  voterAddress: PropTypes.func,
+  voterAddress: PropTypes.func
 };
 
 export default VerifyVoterInfo;
