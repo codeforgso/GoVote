b93d05c65e8d806800a8e3ac8efce02b384da5c7
Schaffer Stewart <SchaeStewart@users.noreply.github.com>
2018-07-15 16:58:11 -0400

    Refactor VoterInfoForm into a reusable component (#97)
    * Implement react-router. Implement basic page design
    
    * Add VoterInfoForm to VoterRegLookup
    
    * Delete modals
    
    * Remove npm commands from contributing guide
    
    * Fix comma dangle
    
    * Add basic pages and implement landing page menu
    
    * Add basic pages and implement landing page menu
    
    * Fix eslint/console warnings
    
    * Refactor VoterInfoForm into reusable component
    
    * Refactor VoterInfoForm into reusable component
    
    * Fix eslint no-alert error

--- src/components/pages/VoterRegLookup.jsx
+++ src/components/pages/VoterRegLookup.jsx
@@ -2,12 +2,74 @@ import React, { Component } from 'react';
 import VoterInfoForm from '../VoterInfoForm';
 
 class VoterRegLookup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      address: '',
+      selectedAddress: {},
+      showUserStatus: false,
+      mayoralCandidates: ['Nancy Vaughan (i)'],
+    };
+  }
+
+  _handleAddressSelect = (selectedAddress) => {
+    this.setState({
+      selectedAddress,
+      showUserStatus: Object.keys(selectedAddress).length > 0,
+    }, () => {
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+  }
+
+  _getVerifiedAddress = (address) => {
+    this.setState({ address });
+  }
+
+  _renderUserStatus = (status) => {
+    const upperStatus = status.toUpperCase();
+    return (
+      <div>
+        <b>{upperStatus}</b>
+      </div>
+    );
+  }
+
+  _renderCandidates = (candidate, index) => (
+    <p key={index}>{candidate}</p>
+  )
+
 
   render() {
     return (
       <div>
         <h1>Are you registered to vote?</h1>
-        <VoterInfoForm />
+        <h4>Enter your name to find your current voter registration status</h4>
+        <VoterInfoForm
+          returnVerifiedVoter={this._handleAddressSelect}
+        />
+        {
+          this.state.showUserStatus ?
+            <div>
+              <br />
+              {this._renderUserStatus(`VOTER REGISTRATION STATUS: ${this.state.selectedAddress.voter_status_desc}`)}
+            </div>
+            : null
+        }
+        {
+          this.state.showUserStatus && this.state.selectedAddress.ward_abbrv ?
+            <div>
+              <br />
+              {this._renderUserStatus(`GREENSBORO CITY COUNCIL DISTRICT: ${this.state.selectedAddress.ward_abbrv.substring(2, 3)}`)}
+              <br />
+              <b>YOUR GREENSBORO CITY COUNCIL CANDIDATES:</b>
+              <br />
+              <br />
+              <b>Mayoral:</b>
+              {this.state.mayoralCandidates.map(this._renderCandidates)}
+              <br />
+            </div>
+            : null
+        }
       </div>
     );
   }
