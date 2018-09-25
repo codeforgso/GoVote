e31838f4a61b2ca6f52f2239e0d4153739b046b7
Schaffer <SchaeStewart@gmail.com>
2018-09-24 21:23:11 -0400

    Fix button layout on small displays on CanIVote

--- src/components/pages/CanIVote.jsx
+++ src/components/pages/CanIVote.jsx
@@ -7,18 +7,16 @@ import CanIVoteRegistered from './CanIVoteRegistered.jsx';
 
 const CanIVote = () => (
   <div>
-    { /* <h1>Can I Vote?</h1> */ }
     <Well>
       <h1>Are you registered to vote?</h1>
       <Row>
-        <Col md={4} sm={4} xs={4}>
+        <Col md={4} sm={12} xs={12}>
           <Link to="/can-i-vote/registered"><Button>Yes, I am registered</Button></Link>
         </Col>
-        <Col md={4} sm={4} xs={4}>
-          { /* Show links to candidates and where vote */}
+        <Col md={4} sm={12} xs={12}>
           <Link to="/can-i-vote/voter-requirements"><Button>No, I am not registered</Button></Link>
         </Col>
-        <Col md={4} sm={4} xs={4}>
+        <Col md={4} sm={12} xs={12}>
           <Link to="/can-i-vote/voter-status"><Button>I am not sure </Button></Link>
         </Col>
       </Row>
