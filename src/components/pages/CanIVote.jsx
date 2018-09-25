0b3ef2134e683a7f2b8b5d5da398c335165e55f6
Schaffer <SchaeStewart@gmail.com>
2018-09-24 21:39:51 -0400

    Update columns for CanIVote buttons

--- src/components/pages/CanIVote.jsx
+++ src/components/pages/CanIVote.jsx
@@ -10,14 +10,14 @@ const CanIVote = () => (
     <Well>
       <h1>Are you registered to vote?</h1>
       <Row>
-        <Col md={4} sm={12} xs={12}>
+        <Col md={4} sm={4} xs={12}>
           <Link to="/can-i-vote/registered"><Button>Yes, I am registered</Button></Link>
         </Col>
-        <Col md={4} sm={12} xs={12}>
+        <Col md={4} sm={4} xs={12}>
           <Link to="/can-i-vote/voter-requirements"><Button>No, I am not registered</Button></Link>
         </Col>
-        <Col md={4} sm={12} xs={12}>
-          <Link to="/can-i-vote/voter-status"><Button>I am not sure </Button></Link>
+        <Col md={4} sm={4} xs={12}>
+          <Link to="/can-i-vote/voter-status"><Button>I am not sure</Button></Link>
         </Col>
       </Row>
     </Well>
