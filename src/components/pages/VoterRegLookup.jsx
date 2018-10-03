d4b5fee5e1a540f57b0449f64745de72fdd9bb58
Schaffer <SchaeStewart@gmail.com>
2018-10-03 12:33:00 -0400

    Refactor VoterInfoForm and VoterRegLookup

--- src/components/pages/VoterRegLookup.jsx
+++ src/components/pages/VoterRegLookup.jsx
@@ -1,60 +1,51 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import VoterInfoForm from '../VoterInfoForm';
+import VoterList from '../VoterList';
 
 class VoterRegLookup extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      address: '',
-      selectedAddress: {},
-      showUserStatus: false,
-      mayoralCandidates: ['Nancy Vaughan (i)'],
+      selectedVoter: {},
+      voterList: [],
+      noVoterList: false,
     };
   }
-
-  _handleAddressSelect = (selectedAddress) => {
-    this.setState({
-      selectedAddress,
-      showUserStatus: Object.keys(selectedAddress).length > 0,
-    }, () => {
-      // window.scrollTo(0, document.body.scrollHeight);
-      window.scrollTo({
-        left: 0,
-        top: 0,
-        behavior: 'smooth',
-      });
-    });
-  }
-
-  _getVerifiedAddress = (address) => {
-    this.setState({ address });
-  }
-
-  _renderUserStatus = (status) => {
-    const upperStatus = status.toUpperCase();
-    return (
-      <div>
-        <b>{upperStatus}</b>
-      </div>
-    );
-  }
-
-  _renderCandidates = (candidate, index) => (
-    <p key={index}>{candidate}</p>
-  )
-
 
   render() {
     return (
       <div>
-        <h1>Are you registered to vote?</h1>
-        <h4>Enter your name to find your current voter registration status</h4>
         <VoterInfoForm
-          returnVerifiedVoter={this._handleAddressSelect}
+          returnVoterList={(voterList) => {
+            this.setState({ voterList, selectedVoter: {} });
+            this.props.returnSelectedVoter({});
+          }}
         />
+        {
+          Object.keys(this.state.selectedVoter).length === 0 ?
+            <VoterList
+              voterList={this.state.voterList}
+              returnSelectedVoter={(selectedVoter) => {
+                this.setState({ selectedVoter });
+                this.props.returnSelectedVoter(selectedVoter);
+              }}
+            />
+          :
+            <ListGroup>
+              <ListGroupItem><b>NAME: </b>{this.state.selectedVoter.first_name} {this.state.selectedVoter.last_name}</ListGroupItem>
+              <ListGroupItem><b>ADDRESS: </b>{this.state.selectedVoter.resident_address}</ListGroupItem>
+              <ListGroupItem><b>REGISTRATION STATUS: </b>{this.state.selectedVoter.voter_status_desc}</ListGroupItem>
+            </ListGroup>
+        }
       </div>
     );
   }
 }
+
+VoterRegLookup.propTypes = {
+  returnSelectedVoter: PropTypes.func.isRequired,
+};
 
 export default VoterRegLookup;
