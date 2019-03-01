ef018f3171c133846ecb5c38999ffa076ebc54fd
Rob Martin <rob.martin.5758@gmail.com>
2019-03-01 13:57:27 -0500

    first changes to add map

---
+++ src/components/VoterPrecintMap.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import VoteHere from '../static/vote.svg';
+import VoteResidence from '../static/home.svg';
+import { handleError } from '../actions';
+
+class GoogleMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapIsReady: false,
+      flipped: null,
+    };
+  }
+
+  componentDidMount() {
+    const GoogleMapAPIKey = process.env.REACT_APP_GOOGLEMAPAPIKEY;
+    // if GoogleMapAPIKey not provided, then no map
+    if (GoogleMapAPIKey) {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${GoogleMapAPIKey}`;
+      script.async = true;
+      script.defer = true;
+      script.addEventListener('load', () => {
+        this.setState({ mapIsReady: true });
+      });
+
+      document.body.appendChild(script);
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.state.mapIsReady) {
+      // create infoWindow object for the text on the marker mouseover
+      this.infoWindow = new window.google.maps.InfoWindow();
+
+      // Display the map
+      this.map = new window.google.maps.Map(document.getElementById('map'), {
+        center: this.props.geocode,
+        zoom: 14,
+        mapTypeId: 'roadmap',
+        map: this.map,
+      });
+      // add marker on the map for the polling place
+      this.voteHereMarker = new window.google.maps.Marker({
+        position: this.props.geocode,
+        titlr: 'Vote Here',
+        map: this.map,
+        icon: {
+          url: VoteHere,
+          scaledSize: new window.google.maps.Size(48, 48),
+        },
+      });
+      // create event to display address on mouseover of the voter polling place
+      window.google.maps.event.addListener(this.voteHereMarker, 'mouseover', () => {
+        this.infoWindow.setContent(`<p>${this.props.pollingPlaceName}</p><p>${this.props.pollingPlaceAddress}</p>`);
+        this.infoWindow.open(this.map, this.voteHereMarker);
+      });
+      window.google.maps.event.addListener(this.voteHereMarker, 'mouseout', () => {
+        this.infoWindow.close(this.map, this.voteHereMarker);
+      });
+
+      // get the geocode for the voter residence and use it to add marker on the map
+      this.state.voterAddressLookup = `${JSON.parse(this.props.voterAddress).res_street_address}, ${JSON.parse(this.props.voterAddress).res_city_desc}, ${JSON.parse(this.props.voterAddress).state_cd}`;
+      this.geocoder = new window.google.maps.Geocoder();
+      this.geocoder.geocode({ address: this.state.voterAddressLookup }, (results, status) => {
+        if (status === 'OK') {
+          this.state.voterResidenceGeocode = results[0].geometry.location;
+          this.voterResidenceMarker = new window.google.maps.Marker({
+            position: results[0].geometry.location,
+            map: this.map,
+            icon: {
+              url: VoteResidence,
+              scaledSize: new window.google.maps.Size(28, 28),
+            },
+          });
+          // create event to display address on mouseover of the voter residence
+          window.google.maps.event.addListener(this.voterResidenceMarker, 'mouseover', () => {
+            this.infoWindow.setContent(`<p>${this.state.voterAddressLookup}</p>`);
+            this.infoWindow.open(this.map, this.voterResidenceMarker);
+          });
+          window.google.maps.event.addListener(this.voterResidenceMarker, 'mouseout', () => {
+            this.infoWindow.close(this.map, this.voterResidenceMarker);
+          });
+        } else {
+          handleError(status);
+        }
+      });
+
+      // add directions on the map.
+      // there is no free tier for this service so the code is commented out for now
+      // *****************************
+      // this.directionsService = new window.google.maps.DirectionsService();
+      // this.directionsService.route({ origin: this.state.voterResidenceGeocode, destination: this.props.geocode, travelMode: 'DRIVING' }, (response, status) => {
+      //   if (status === 'OK') {
+      //     this.map.directionsDisplay.setDirections(response);
+      //   }
+      // });
+      // *****************************
+    }
+  }
+
+  render() {
+    return (
+      <div id="map" style={{ width: '100%', height: '50vh' }} />
+    );
+  }
+}
+
+GoogleMap.propTypes = {
+  geocode: PropTypes.object.isRequired,
+  voterAddress: PropTypes.string.isRequired,
+  pollingPlaceName: PropTypes.string.isRequired,
+  pollingPlaceAddress: PropTypes.string.isRequired,
+};
+
+module.exports = GoogleMap;
--- src/components/pages/WhereAndWhen.jsx
+++ src/components/pages/WhereAndWhen.jsx
@@ -3,6 +3,7 @@ import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import axios from 'axios';
 import { handleError } from '../../actions';
 import VoterRegLookup from '../VoterRegLookup';
+import VoterPrecintMap from '../VoterPrecintMap';
 
 class WhereAndWhen extends React.Component {
   constructor() {
@@ -14,71 +15,100 @@ class WhereAndWhen extends React.Component {
     };
   }
 
-  _getPolingPlaceInfo = (voter) => {
+  _getPollingPlaceInfo = (voter) => {
     if (Object.keys(voter).length === 0) {
-      this.setState({ pollingPlace: {} });
+      this.setState({ pollingPlace: {}, geocode: {} });
       return;
     }
 
     const precinctDesc = voter.precinct_desc;
     // const url = `http://gis.co.guilford.nc.us/arcgis/rest/services/Elections/Elections/MapServer/0/query?where=UPPER(PRECINCT)%20like%20%27%25${precinctDesc}%25%27&outFields=*&outSR=4326&f=json`;
     const url = `http://gis.guilfordcountync.gov/arcgis/rest/services/Elections/Elections/MapServer/0/query?where=UPPER(PRECINCT)%20like%20%27%25${precinctDesc}%25%27&outFields=*&outSR=4326&f=json`;
+
     this.setState({ isLoading: true });
-    axios.get(url)
+    axios
+      .get(url)
       .then((response) => {
         const { attributes } = response.data.features[0];
+        const { geometry } = response.data.features[0];
         const pollingPlace = {
           name: attributes.POLLING_PLACE,
           address: `${attributes.ADDRESS}, ${attributes.CITY}, NC`,
         };
-        this.setState({ pollingPlace, isLoading: false });
+        const geocode = { lng: geometry.x, lat: geometry.y };
+        this.setState({ pollingPlace, isLoading: false, geocode });
       })
       .catch((error) => {
         handleError(error);
+        // eslint-disable-next-line no-console
+        console.error(error);
         this.setState({ errorGettingPollingPlace: true });
       })
-      .finally(() => { window.scrollTo(0, document.body.scrollHeight); });
-  }
+      .finally(() => {
+        window.scrollTo(0, document.body.scrollHeight);
+      });
+  };
 
-  _createGoogleMapsLink = address => (`https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(address)}`);
+
+  _createVoterPrecintMapsLink = address =>
+    `https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(
+      address
+    )}`;
 
   render() {
-    const { pollingPlace, isLoading, errorGettingPollingPlace } = this.state;
+    const { pollingPlace, isLoading, errorGettingPollingPlace, geocode } = this.state;
     return (
       <div>
         <h1>Where to Vote</h1>
         <p>Enter your information below to find out where to vote</p>
-        <VoterRegLookup
-          returnSelectedVoter={this._getPolingPlaceInfo}
-        />
-        {
-          isLoading && <span>Getting Polling Place Info</span>
-        }
-        {
-          !isLoading && Object.keys(pollingPlace).length > 0 ?
-            <ListGroup>
-              <ListGroupItem><b>Polling Place Name:</b> {pollingPlace.name}</ListGroupItem>
-              <ListGroupItem>
-                <b>Polling Place Address: </b>
-                <a
-                  target="_blank"
-                  href={this._createGoogleMapsLink(pollingPlace.address)}
-                  title="View on Google Maps"
-                >
-                  {pollingPlace.address}
-                </a>
-              </ListGroupItem>
-            </ListGroup>
-          : null
-        }
-        {
-          errorGettingPollingPlace && <span>Error getting polling place info. Please try again</span>
-        }
+        <VoterRegLookup returnSelectedVoter={this._getPollingPlaceInfo} />
+        {isLoading && <span>Getting Polling Place Info</span>}
+        {!isLoading && Object.keys(pollingPlace).length > 0 ? (
+          <ListGroup>
+            <ListGroupItem>
+              <b>Polling Place Name:</b> {pollingPlace.name}
+            </ListGroupItem>
+            <ListGroupItem>
+              <b>Polling Place Address: </b>
+              <a
+                target="_blank"
+                href={this._createVoterPrecintMapsLink(pollingPlace.address)}
+                title="View on Google Maps"
+              >
+                {pollingPlace.address}
+              </a>
+            </ListGroupItem>
+            <ListGroupItem>
+              <VoterPrecintMap geocode={geocode} voterAddress={window.sessionStorage.getItem('VoterRegLookupSelectedVoter')} pollingPlaceAddress={pollingPlace.address} pollingPlaceName={pollingPlace.name}>
+              </VoterPrecintMap>
+            </ListGroupItem>
+          </ListGroup>
+        ) : null}
+        {errorGettingPollingPlace && (
+          <span> Error getting polling place info. Please try again</span>
+        )}
         <h1>When to Vote</h1>
-        <p><a href="https://www.guilfordcountync.gov/home/showdocument?id=4845">Guilford County Board of Elections One-Stop Early Voting Schedule November 6, 2018 General Elections</a></p>
-        <p>Election Day is Tuesday, November 6th and you can vote from 6:30am to 7:30 pm at your assigned polling place. Remember to register to vote by October 12th if you want to vote on Election Day!</p>
+        <p>
+          <a href="https://www.guilfordcountync.gov/home/showdocument?id=4845">
+            Guilford County Board of Elections One-Stop Early Voting Schedule
+            November 6, 2018 General Elections
+          </a>
+        </p>
+        <p>
+          Election Day is Tuesday, November 6th and you can vote from 6:30am to
+          7:30 pm at your assigned polling place. Remember to register to vote
+          by October 12th if you want to vote on Election Day!
+        </p>
 
-        <p>Early Voting (including &quot;One-Stop Early Voting&quot;, which allows you to register and vote at the same time) begins Wednesday, October 17th and runs through Saturday, November 3rd. However, different restrictions on time apply to different polling sites around Guilford County. Most early voting sites are open business hours on weekdays Click here to view the County&#29;s comprehensive calendar that lists hours at each location.</p>
+        <p>
+          Early Voting (including &quot;One-Stop Early Voting&quot;, which
+          allows you to register and vote at the same time) begins Wednesday,
+          October 17th and runs through Saturday, November 3rd. However,
+          different restrictions on time apply to different polling sites around
+          Guilford County. Most early voting sites are open business hours on
+          weekdays Click here to view the County&#29;s comprehensive calendar
+          that lists hours at each location.
+        </p>
       </div>
     );
   }
