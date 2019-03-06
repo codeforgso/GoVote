9d5fd5da8390af818583de9d71dcf05abd473026
dresean <williamsdresean@gmail.com>
2019-03-05 20:12:42 -0500

    change h2 to p tag

--- src/components/pages/DayOfInfo.jsx
+++ src/components/pages/DayOfInfo.jsx
@@ -6,7 +6,8 @@ const DayOfInfo = () => (
     <h1>It{'\''}s Election Day, What Do I Do?</h1>
     <h2>What to Expect</h2>
     <h2>How to Report Issues With Your Polling Place</h2>
-    <Link to="/where-and-when"><h2>Look Up Your Polling Place</h2></Link>
+    <h2>Polling Place</h2>
+    <Link to="/where-and-when"><p>Look Up Your Polling</p></Link>
   </div>
   );
 
