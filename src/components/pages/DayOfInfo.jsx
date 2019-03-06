c374ed2575707fdc0d9a0b78746d505354f497b4
dresean <williamsdresean@gmail.com>
2019-03-05 20:00:00 -0500

    add link to polling places

--- src/components/pages/DayOfInfo.jsx
+++ src/components/pages/DayOfInfo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const DayOfInfo = () => (
   <div>
     <h1>It{'\''}s Election Day, What Do I Do?</h1>
     <h2>What to Expect</h2>
     <h2>How to Report Issues With Your Polling Place</h2>
-    <h2>Polling Place</h2>
+    <Link to="/where-and-when"><h2>Look Up Your Polling Place</h2></Link>
   </div>
   );
 
