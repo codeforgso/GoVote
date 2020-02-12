54d884883708cd6800ae8f1b64e0532c592e0d82
Teddy Caddy <tcaddy@users.noreply.github.com>
2020-02-11 20:32:34 -0500

    Better Polling Place data usage (#156)
    Return results even when `polling_places` record cannot be found
    
    Use `LEFT OUTER JOIN` vs `INNER JOIN` on `polling_places` table
     * Allows you to at least get results from the `voters` table to the UI
    
    Use `COALESCE()` to provide alternate results when `NULL`
    
    Update polling place URL to a newer dataset
    
    More garbage data cleanup from CSV files on ETL job

--- server/server.js
+++ server/server.js
@@ -35,15 +35,18 @@ router.get('/api/:fn/:ln', async (req, res) => {
   const pollingTable = 'polling_places';
   const query = `SELECT ${voterTable}.*,
     polling_place_id,
-    polling_place_name,
-    precinct_name as "polling_place_precinct_name",
-    house_num     as "polling_place_house_num",
-    street_name   as "polling_place_street_name",
-    city          as "polling_place_city",
-    state         as "polling_place_state",
-    zip           as "polling_place_zip"
+    COALESCE(
+      polling_place_name, 'Unable to lookup polling place name'
+    ) AS polling_place_name,
+    COALESCE(precinct_name, '') AS polling_place_precinct_name,
+    COALESCE(house_num, '')     AS polling_place_house_num,
+    COALESCE(street_name, '')   AS polling_place_street_name,
+    COALESCE(city, '')          AS polling_place_city,
+    COALESCE(state, '')         AS polling_place_state,
+    COALESCE(zip, '')           AS polling_place_zip
     FROM ${voterTable}
-    INNER JOIN ${pollingTable} ON ${voterTable}.precinct_abbrv = ${pollingTable}.precinct_name 
+    LEFT OUTER JOIN ${pollingTable}
+      ON ${voterTable}.precinct_abbrv = ${pollingTable}.precinct_name
       AND ${voterTable}.county_desc = ${pollingTable}.county_name
     WHERE voter_status_desc NOT LIKE 'REMOVED'
       AND first_name ilike $1::text
